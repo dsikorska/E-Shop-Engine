618008dc65b09e64f81acb233c1a8f929bb66527
LadyHail <ladydeath@o2.pl>
2018-08-25 22:19:32 +0200

    Implementation of subcategory controller and views - Admin Area

--- E-Shop-Engine.Website/Areas/Admin/Controllers/SubcategoryController.cs
+++ E-Shop-Engine.Website/Areas/Admin/Controllers/SubcategoryController.cs
@@ -2,40 +2,46 @@
 using System.Web.Mvc;
 using E_Shop_Engine.Domain.DomainModel;
 using E_Shop_Engine.Domain.Interfaces;
+using E_Shop_Engine.Website.Areas.Admin.Models;
 
 namespace E_Shop_Engine.Website.Areas.Admin.Controllers
 {
-    public class CategoryController : Controller
+    public class SubcategoryController : Controller
     {
+        private readonly IRepository<Subcategory> _subcategoryRepository;
         private readonly IRepository<Category> _categoryRepository;
 
-        public CategoryController(IRepository<Category> categoryRepository)
+        public SubcategoryController(IRepository<Subcategory> subcategoryRepository, IRepository<Category> categoryRepository)
         {
+            _subcategoryRepository = subcategoryRepository;
             _categoryRepository = categoryRepository;
         }
-        //TODO: add validation and routing
-        // GET: Admin/Category
+
+        // GET: Admin/Subcategory
         [HttpGet]
         public ActionResult Index()
         {
-            return View(_categoryRepository.GetAll());
+            return View(_subcategoryRepository.GetAll());
         }
 
         [HttpGet]
         public ViewResult Edit(int id)
         {
-            return View(_categoryRepository.GetById(id));
+            SubcategoryViewModel model = _subcategoryRepository.GetById(id);
+            model.Categories = _categoryRepository.GetAll();
+
+            return View(model);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Category model)
+        public ActionResult Edit(Subcategory model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
-            _categoryRepository.Update(model);
+            _subcategoryRepository.Update(model);
 
             return RedirectToAction("Index");
         }
@@ -43,36 +49,39 @@ namespace E_Shop_Engine.Website.Areas.Admin.Controllers
         [HttpGet]
         public ViewResult Create()
         {
-            return View("Edit", new Category());
+            SubcategoryViewModel model = new SubcategoryViewModel();
+            model.Categories = _categoryRepository.GetAll();
+
+            return View("Edit", model);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Category model)
+        public ActionResult Create(Subcategory model)
         {
             if (!ModelState.IsValid)
             {
                 return View("Edit", model);
             }
-            _categoryRepository.Create(model);
+            _subcategoryRepository.Create(model);
             return RedirectToAction("Index");
         }
 
         [HttpGet]
         public ActionResult Details(int id)
         {
-            return View(_categoryRepository.GetById(id));
+            return View(_subcategoryRepository.GetById(id));
         }
 
         public ActionResult Delete(int id)
         {
             try
             {
-                _categoryRepository.Delete(id);
+                _subcategoryRepository.Delete(id);
             }
             catch (DbUpdateException e)
             {
-
+                //all products needs to be move to another subcategory
             }
 
             return RedirectToAction("Index");
---
+++ E-Shop-Engine.Website/Areas/Admin/Models/SubcategoryViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using E_Shop_Engine.Domain.DomainModel;
+
+namespace E_Shop_Engine.Website.Areas.Admin.Models
+{
+    public class SubcategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryID { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public static implicit operator SubcategoryViewModel(Subcategory model)
+        {
+            return new SubcategoryViewModel
+            {
+                Name = model.Name,
+                Description = model.Description,
+                CategoryID = model.CategoryID,
+                Id = model.ID
+
+            };
+        }
+
+        public static implicit operator Subcategory(SubcategoryViewModel model)
+        {
+            return new Subcategory
+            {
+                Name = model.Name,
+                Description = model.Description,
+                CategoryID = model.CategoryID,
+                ID = model.Id
+            };
+        }
+    }
+}
